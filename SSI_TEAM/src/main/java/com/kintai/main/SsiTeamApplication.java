e3f8c2f0c587e59bcc7ef1c4ea61435409838838
shuny <shuny@LAPTOP-D1E6JPHD>
2023-08-08 14:16:04 +0900

    aaaa

--- SSI_TEAM/src/main/java/com/kintai/main/SsiTeamApplication.java
+++ SSI_TEAM/src/main/java/com/kintai/main/SsiTeamApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SsiTeamApplication {
 
-	//こんにちは！
+	//こんにちは！aaaa
 	public static void main(String[] args) {
 		SpringApplication.run(SsiTeamApplication.class, args);
 	}
