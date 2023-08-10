5357809ce11b7bc8723f92706bd7e5616b2bd6d4
Shinozaki973131 <mmm973131@gmail.com>
2023-08-10 09:08:14 +0900

    更新しました。

--- SSI_TEAM/src/main/java/com/kintai/main/entity/Employee_r_dao.java
+++ SSI_TEAM/src/main/java/com/kintai/main/entity/Employee_r_dao.java
@@ -2,11 +2,14 @@ package com.kintai.main.entity;
 
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Data;
 @Data
 @Entity
@@ -24,7 +27,28 @@ public class Employee_r_dao {
 	    private Date NYUSHA_DATE;
 	    private int AGE;
 	    private String MAIL_ADDRESS;
-	    
+	    @Column(name = "CREATE_DATE")
+	    private Date CREATE_DATE;
+
+	    @Column(name = "CREATE_USER")
+	    private String CREATE_USER;
+
+	    @Column(name = "UPDATE_DATE")
+	    private Date UPDATE_DATE;
+
+	    @Column(name = "UPDATE_USER")
+	    private String UPDATE_USER;
+	    @PrePersist
+	    protected void onCreate() {
+	        CREATE_DATE = new Date();
+	        // ログインユーザーIDを設定するコードをここに追加
+	    }
+
+	    @PreUpdate
+	    protected void onUpdate() {
+	        UPDATE_DATE = new Date();
+	        // ログインユーザーIDを設定するコードをここに追加
+	    }
 	    // 他のフィールド
 	    // getters and setters
 	}
