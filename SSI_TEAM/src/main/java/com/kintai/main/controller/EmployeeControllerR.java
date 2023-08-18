c8e742b6f86a1b9088f6e280ab155a4499f5e4d8
Shinozaki973131 <mmm973131@gmail.com>
2023-08-18 12:53:39 +0900

    自分が追加した、アプリケーションプロパティーズのhibernate設定の削除、自作のリポジトリクラスを削除、その他自作クラスの変数名修正。

--- SSI_TEAM/src/main/java/com/kintai/main/controller/EmployeeControllerR.java
+++ SSI_TEAM/src/main/java/com/kintai/main/controller/EmployeeControllerR.java
@@ -1,16 +1,13 @@
 //篠崎作成
 package com.kintai.main.controller;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kintai.main.entity.EmployeeDao;
+import com.kintai.main.entity.Employee;
 import com.kintai.main.service.EmployeeServiceR;
 
 @Controller
@@ -26,28 +23,9 @@ public class EmployeeControllerR {
 
     @GetMapping("/form")
     public String showEmployeeForm(Model model) {
-        // isUpdateを設定してフォームに渡す
+        Employee employee = new Employee(); // Employee オブジェクトの初期化（適切な初期値を設定する）
+        model.addAttribute("employee", employee); // モデルに追加
         model.addAttribute("isUpdate", true); // 例えば更新リンクが押された場合
-        return "employee-form";
-    }
-
-    @PostMapping("/insert")
-    public String insertEmployee(EmployeeDao employee) {
-        // 作成日時を設定
-        employee.setCREATE_DATE(new Date());
-
-        // 登録処理を実行
-        employeeService.insertEmployee(employee);
-        return "redirect:/employees/form";
-    }
-
-    @PostMapping("/update")
-    public String updateEmployee(EmployeeDao employee) {
-        // 更新日時を設定
-        employee.setUPDATE_DATE(new Date());
-
-        // 更新処理を実行
-        employeeService.updateEmployee(employee);
-        return "redirect:/employees/form";
+        return "employee_info_register";
     }
 }
--- SSI_TEAM/src/main/java/com/kintai/main/entity/EmployeeDao.java
+++ SSI_TEAM/src/main/java/com/kintai/main/entity/EmployeeDao.java
@@ -2,79 +2,73 @@ package com.kintai.main.entity;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
 
 import lombok.Data;
 
 @Data
-@Entity(name = "t_employee") // テーブル名
+
 public class EmployeeDao {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    
     private Long id;
     
-    @Column(name = "employee_id",length =10) // カラム名と一致させる
-    private String EMPLOYEE_ID;
-    @Column(name = "password",length =20)
-    private String PASSWORD;
     
-    @Column(name = "employee_name",length =100) // カラム名と一致させる
-    private String EMPLOYEE_NAME;
+    private String employeeId;
     
-    @Column(name = "dept_id",length =10) // カラム名と一致させる
-    private String DEPT_ID;
-    @Column(name = "access",length =1)
-    private String ACCESS;
+    private String password;
     
-    @Column(name = "sex_id",length =1) // カラム名と一致させる
-    private String SEX_ID;
-    @Column(name = "age",length =3)
-    private int AGE;
+   
+    private String employeeName;
     
-    @Column(name = "tel_no",length =11) // カラム名と一致させる
-    private String TEL_NO;
+  
+    private String deptId;
+  
+    private String access;
     
-    @Column(name = "mail_address",length =100) // カラム名と一致させる
-    private String MAIL_ADDRESS;
+   
+    private String sexId;
+   
+    private int age;
     
-    @Column(name ="nyusha_date")
-    private Date NYUSHA_DATE;
     
-    @Column(name = "del_flg",length = 1) // カラム名と一致させる
-    private String DEL_FLG;
+    private String telNo;
     
-    @Column(name = "create_date") // カラム名と一致させる
-    private Date CREATE_DATE;
+  
+    private String mailAddress;
+    
+   
+    private Date nyushaDate;
+    
+   
+    private String delFlg;
+    
+   
+    private Date createDate;
 
-    @Column(name = "create_user",length =10) // カラム名と一致させる
-    private String CREATE_USER;
+    
+    private String createUser;
 
-    @Column(name = "update_date") // カラム名と一致させる
-    private Date UPDATE_DATE;
+    
+    private Date updateDate;
 
-    @Column(name = "update_user",length = 10) // カラム名と一致させる
-    private String UPDATE_USER;
+   
+    private String updateUser;
     
 
 
-    @PrePersist
-    protected void onCreate() {
-        CREATE_DATE = new Date();
-        // ログインユーザーIDを設定するコードをここに追加
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        UPDATE_DATE = new Date();
-        // ログインユーザーIDを設定するコードをここに追加
-    }
+//    @PrePersist
+//    protected void onCreate() {
+//        CREATE_DATE = new Date();
+//        // ログインユーザーIDを設定するコードをここに追加
+//    }
+//
+//    @PreUpdate
+//    protected void onUpdate() {
+//        UPDATE_DATE = new Date();
+//        // ログインユーザーIDを設定するコードをここに追加
+//    }
     // 他のフィールド
     // getters and setters
 }
