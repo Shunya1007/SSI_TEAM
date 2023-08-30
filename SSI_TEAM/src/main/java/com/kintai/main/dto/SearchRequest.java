ef30dbc6f6747500da76244eb386335cb51daf6a
shuny <shuny@LAPTOP-D1E6JPHD>
2023-08-30 14:53:34 +0900

    社員情報一覧画面社員IDクリックで遷移出来る様に修正

--- SSI_TEAM/src/main/java/com/kintai/main/dto/SearchRequest.java
+++ SSI_TEAM/src/main/java/com/kintai/main/dto/SearchRequest.java
@@ -1,6 +1,9 @@
 package com.kintai.main.dto;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
@@ -9,5 +12,9 @@ public class SearchRequest implements Serializable{
 	private String employeeId;
 	
 	private String deptId;
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate nyushaDates;
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate nyushaDatee;
 	
 }
